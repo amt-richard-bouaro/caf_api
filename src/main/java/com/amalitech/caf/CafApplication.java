ad58bf461f36af0a7b98ab09446d2ac45daeb639
richard-bouaro <richard.bouaro@amalitech.com>
2024-01-25 11:23:39 +0000

    chore: initial setup

---
+++ src/main/java/com/amalitech/caf/CafApplication.java
@@ -0,0 +1,13 @@
+package com.amalitech.caf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalitech/caf/CafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalitech.caf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
